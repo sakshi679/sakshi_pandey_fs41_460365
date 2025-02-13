db29f0b26bcca282c6c1fc35cff1cb72ec4d68d5
sakshi679 <pandeysai595@gmail.com>
2025-02-13 12:59:13 +0530

    Add files via upload

---
+++ L0 - Data Privacy using Closures.js
@@ -0,0 +1,19 @@
+function createCounter() {
+    let count = 0
+    return {
+    increment: function(){
+    count++
+    return count
+    },
+    getcount: function() {
+    return count
+    }
+    };
+    }
+    const counter = createCounter();
+
+    console.log(counter.increment()); // Output: 1;
+    
+    console.log(counter.increment()); // Output: 2;
+    
+    console.log(counter.getCount()); // Output: 2;
