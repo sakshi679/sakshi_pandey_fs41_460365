7b0007e6530b3b50e550dd9ca8770201cbc51493
sakshi679 <pandeysai595@gmail.com>
2025-02-13 12:56:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-let squareandcube = num => (
-    {square: num**2, cube: num**3}
-);
-console.log(squareandcube(5));
+function createCounter() {
+    let count = 0
+    return {
+    increment: function(){
+    count++
+    return count
+    },
+    getcount: function() {
+    return count
+    }
+    };
+    }
+    const counter = createCounter();
+
+    console.log(counter.increment()); // Output: 1;
+    
+    console.log(counter.increment()); // Output: 2;
+    
+    console.log(counter.getCount()); // Output: 2;    
